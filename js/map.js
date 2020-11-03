fe9017beba97b07bb9c6852f1e287c24fdb2ae38
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-11-03 14:31:43 +0200

    init

--- js/map.js
+++ js/map.js
@@ -13,7 +13,7 @@ var stops_values = [
     [3, "#feb24c"],
     [6, "#fd8d3c"],
     [8, "#f03b20"],
-    [10, "#bd0026"]
+    [60, "#bd0026"]
 ];
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHJpbWFjdXMxODIiLCJhIjoiWGQ5TFJuayJ9.6sQHpjf_UDLXtEsz8MnjXw';
@@ -30,65 +30,95 @@ var map = new mapboxgl.Map({
     zoom: default_zoom_u // starting zoom
 });
 
+
+
 map.scrollZoom.disable();
 
 
-    /* ------- карта України ------- */
-    map.on('load', function () {
-
-        //векторні тайли
-        map.addSource('schools', {
-            type: 'vector',
-            tiles: ["https://texty.github.io/datasets/covid_parties/tiles/{z}/{x}/{y}.pbf"]
-        });
 
 
-        function redrawUkraineMap(choropleth_column) {
-            map.addLayer({
-                "id": "schools_data",
-                'type': 'fill',
-                'minzoom': 5,
-                'maxzoom': 10,
-                'source': "schools",
-                "source-layer": "for_tiles_4326.geojson",
-                "paint": {
-                    'fill-color': {
-                        property: choropleth_column,
-                        stops: stops_values
-                    },
+/* ------- карта України ------- */
+map.on('load', function () {
 
 
-                    'fill-outline-color': [
-                        'case',
-                        ['boolean', ['feature-state', 'hover'], false],
-                        "grey",
-                        "lightgrey"
-                    ]
-                }
-            });
+    var layers = map.getStyle().layers;
+    var firstSymbolId;
+
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol') {
+            firstSymbolId = layers[i].id;
+            break;
         }
-
-        map.on('click', 'schools_data', function(e) {
-           map.getCanvas().style.cursor = 'pointer';
-           new mapboxgl.Popup()
-                .setLngLat(e.lngLat)
-                .setHTML(e.features[0].properties.MAP_registration_region)
-                .addTo(map);
-        });
-
-        redrawUkraineMap('MAP_infections1000');
-
-        /* перемикаємо шари  карти */
-        d3.select("#ukraine-switch-buttons").selectAll(".map_button").on("click", function() {
-            let selected_layer = d3.select(this).attr("value");
-            d3.select(this.parentNode).selectAll(".map_button").classed("active", false);
-            d3.select(this).classed("active", true);
-            map.removeLayer('schools_data');
-            redrawUkraineMap(selected_layer);
-        });
+    }
 
 
-        var nav = new mapboxgl.NavigationControl();
-        map.addControl(nav, 'top-left');
 
-    }); //end of Ukraine map
+    //векторні тайли
+    map.addSource('schools', {
+        type: 'vector',
+        tiles: ["https://texty.github.io/covid_leisure/tiles/{z}/{x}/{y}.pbf"]
+    });
+
+
+    function redrawUkraineMap(choropleth_column) {
+        map.addLayer({
+            "id": "schools_data",
+            'type': 'fill',
+            'minzoom': 4,
+            'maxzoom': 10,
+            'source': "schools",
+            "source-layer": "covid_leisure_4326",
+            "paint": {
+                'fill-color': {
+                    property: choropleth_column,
+                    stops: stops_values
+                },
+
+
+                'fill-outline-color': [
+                    'case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    "grey",
+                    "lightgrey"
+                ]
+            }
+        }, firstSymbolId);
+    }
+
+    map.on('click', 'schools_data', function(e) {
+        map.getCanvas().style.cursor = 'pointer';
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.MAP_registration_region)
+            .addTo(map);
+    });
+
+    redrawUkraineMap('MAP_infections1000');
+
+    /* перемикаємо шари  карти */
+    d3.select("#ukraine-switch-buttons").selectAll(".map_button").on("click", function() {
+        let selected_layer = d3.select(this).attr("value");
+        d3.select(this.parentNode).selectAll(".map_button").classed("active", false);
+        d3.select(this).classed("active", true);
+        map.removeLayer('schools_data');
+        redrawUkraineMap(selected_layer);
+    });
+
+
+    var nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-left');
+
+}); //end of Ukraine map
+
+
+
+
+
+
+
+
+
+
+
+
+
